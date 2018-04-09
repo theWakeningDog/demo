40d0b20a48de49237f6012288e30bd56f734bc4b
zw <501755286@qq.com>
2018-04-09 16:45:55 +0800

    调用在不同包下的bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.example.demo2.SpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 /*
 * 让spring boot自动给程序进行必要的配置，等价于以默认属性使用
@@ -10,7 +13,17 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 * */
 //@ServletComponentScan
 @SpringBootApplication
+@Import(value=SpringUtil.class)   //这是第三种，扫描不在包下的springutil
 public class DemoApplication {
+
+	/*
+	* 方式二
+	* 注册springutil，在demo2中的
+	* */
+//	@Bean
+//	public SpringUtil springUtil2() {
+//		return new SpringUtil();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo2/SpringUtil.java
+++ src/main/java/com/example/demo2/SpringUtil.java
@@ -1,7 +1,55 @@
 package com.example.demo2;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
- * Created by zhangwei on 2018/4/7.
+ * 外部类普通类调用Spring bean对象，不在同包或子包下
+ *
+ * 说明：此类需要放到App.java同包或子包下才能被扫描
+ * Created by zhangwei on 2018/4/6 0006.
  */
-public class SpringUtil {
+public class SpringUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext = null;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtil.applicationContext == null) {
+            SpringUtil.applicationContext = applicationContext;
+        }
+        System.out.println("------------------------------------------------");
+        System.out.println("demo2中ApplicationContext配置成功,在普通类可以通过调用SpringUtils.getAppContext()获取" +
+                "applicationContext对象,applicationContext="+SpringUtil.applicationContext);
+        System.out.println("------------------------------------------------");
+    }
+
+    /*
+     * 获取applicationContext
+     * */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /*
+     * 通过name获取bean
+     * */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /*
+     * 通过clazz获取Bean
+     * */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /*
+     * 通过name及clazz返回制定的Bean
+     * */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
 }
