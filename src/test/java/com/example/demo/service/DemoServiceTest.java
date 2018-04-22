373ada608001959388dca7a7d4f789c324c8de61
zw <501755286@qq.com>
2018-04-22 13:28:55 +0800

    单元测试

---
+++ src/test/java/com/example/demo/service/DemoServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by ZhangWei on 2018/4/22 10:21.
+ */
+//SpringJUnit4支持，由此引入Spring-Test框架支持
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class DemoServiceTest {
+}
