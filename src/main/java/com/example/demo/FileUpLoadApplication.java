5ffc7c77dfb78322683d1be4bcc47429d65992e2
zw <501755286@qq.com>
2018-04-26 21:28:44 +0800

    多文件上传

--- src/main/java/com/example/demo/FileUpLoadApplication.java
+++ src/main/java/com/example/demo/FileUpLoadApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created by ZhangWei on 2018/4/23 15:34.
  */
 @SpringBootApplication
 public class FileUpLoadApplication {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //限制上传数据大小，超了会抛出异常信息
+        factory.setMaxFileSize("100KB");
+        //设置总上传数据大小
+        factory.setMaxRequestSize("200KB");
+        factory.setLocation("路径地址");
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FileUpLoadApplication.class, args);
--- src/main/java/com/example/demo/fileUpLoad/FileUpLoadController.java
+++ src/main/java/com/example/demo/fileUpLoad/FileUpLoadController.java
@@ -2,11 +2,16 @@ package com.example.demo.fileUpLoad;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ZhangWei on 2018/4/22 16:45.
@@ -17,7 +22,6 @@ public class FileUpLoadController {
 
     @RequestMapping("/file")
     public  String file() {
-        System.out.println("11111111111111111111111111111111111111");
         return "/fileUpLoad/file.html";
     }
 
@@ -41,5 +45,35 @@ public class FileUpLoadController {
             return "上传成功";
         }
         return "上传失败，无文件";
+    }
+
+    @RequestMapping("/moreFile")
+    public String moreFile() {
+        return "/fileUpLoad/moreFile";
+    }
+
+    @RequestMapping(value = "/more", method = RequestMethod.POST)
+    @ResponseBody
+    public String UpLoadMoreFile(HttpServletRequest request) {
+        List<MultipartFile> fileList = ((MultipartHttpServletRequest)request).getFiles("file");
+        MultipartFile multipartFile = null;
+        BufferedOutputStream out = null;
+        for (int i = 0; i < fileList.size(); ++i) {
+            multipartFile = fileList.get(i);
+            if (!multipartFile.isEmpty()) {
+                try {
+                    byte[] bytes = multipartFile.getBytes();
+                    out = new BufferedOutputStream(new FileOutputStream(new File(multipartFile.getOriginalFilename())));
+                    out.write(bytes);
+                    out.close();
+                } catch (IOException e) {
+                    out = null;
+                    return"You failed to upload " + i + " => " + e.getMessage();
+                }
+            } else {
+                return"You failed to upload " + i + " because the file was empty.";
+            }
+        }
+        return "上传成功";
     }
 }
